6bcf1580da83cf69240dcaa0ae046d580f0368b1
Maxim Sidorov <s_maxim_v@bk.ru>
2022-04-27 17:03:39 +0300

    init xcode project

---
+++ PrompterHints/PrompterHintsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrompterHintsApp.swift
+//  PrompterHints
+//
+//  Created by MSP on 27.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct PrompterHintsApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
